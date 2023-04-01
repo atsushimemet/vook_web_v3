0fa42a7dbc9d35d8668b201126564a71e65cb127
garammasala29 <kmasa1569@gmail.com>
2023-04-01 13:38:24 +0900

    feat: #1 RSpecとrubocopの設定

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,11 @@ module VookWebV3
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
