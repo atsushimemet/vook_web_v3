1f4b46e5926767ae502b8fb976671a66601cf072
garammasala29 <kmasa1569@gmail.com>
2023-04-19 08:43:55 +0900

    feat: #14 Magazineのシステムスペックを追加

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,6 +1,5 @@
 class Magazine < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
-  validates :body, presence: true
   validates :publish_at, presence: true
 
   has_one_attached :thumbnail
--- spec/factories/magazines.rb
+++ spec/factories/magazines.rb
@@ -1,8 +1,43 @@
 FactoryBot.define do
   factory :magazine do
-    title { 'MyString' }
-    body { 'MyText' }
-    summary { 'MyString' }
-    publish_at { '2023-04-12 22:25:20' }
+    title { '【購入品紹介】"French Navy" Deck Jacket' }
+    publish_at { Date.current }
+    thumbnail { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/map.jpg'), 'image/jpg') }
+    after(:create) do |magazine|
+      ActionText::RichText.create!(
+        record_type: 'Magazine',
+        record_id: magazine.id,
+        name: 'body',
+        body: "<div class=\"trix-content\">\n  <div>90年代のフランス海軍デッキジャケット</div>\n</div>\n"
+      )
+    end
+  end
+
+  factory :yesterday_magazine, class: 'Magazine' do
+    title { '公開日が機能の記事' }
+    publish_at { Date.current - 1 }
+    thumbnail { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/map.jpg'), 'image/jpg') }
+    after(:create) do |magazine|
+      ActionText::RichText.create!(
+        record_type: 'Magazine',
+        record_id: magazine.id,
+        name: 'body',
+        body: "<div class=\"trix-content\">\n  <div>`#{Date.current - 1}`の記事</div>\n</div>\n"
+      )
+    end
+  end
+
+  factory :tomorrow_magazine, class: 'Magazine' do
+    title { '公開日が明日の記事' }
+    publish_at { Date.current + 1 }
+    thumbnail { Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/map.jpg'), 'image/jpg') }
+    after(:create) do |magazine|
+      ActionText::RichText.create!(
+        record_type: 'Magazine',
+        record_id: magazine.id,
+        name: 'body',
+        body: "<div class=\"trix-content\">\n  <div>`#{Date.current + 1}`の記事</div>\n</div>\n"
+      )
+    end
   end
 end
