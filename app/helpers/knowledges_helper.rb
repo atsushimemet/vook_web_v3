4aedb6779907b29cc35062f9a5e889e34a55f4e6
garammasala29 <kmasa1569@gmail.com>
2023-07-01 08:20:54 +0900

    feat: #55 バリデーションエラーが出た際にも値を保持できるよう設定

--- app/helpers/knowledges_helper.rb
+++ app/helpers/knowledges_helper.rb
@@ -4,6 +4,6 @@ module KnowledgesHelper
   end
 
   def line_select_options(knowledge)
-    knowledge.brand.try(:line).to_a.map { |line| [line.name, line.id] }
+    Line.where(brand_id: knowledge.brand_id).map { |line| [line.name, line.id] }
   end
 end
