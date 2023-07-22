fff1e489872132bd3f0ca57cc6226cabe1f28435
garammasala29 <kmasa1569@gmail.com>
2023-07-22 14:33:45 +0900

    close #86

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @brands = Brand.all
     @items = Item.all
-    @magazines = Magazine.with_attached_thumbnail.order(publish_at: :desc).limit(4)
+    @magazines = Magazine.with_attached_thumbnail.order(publish_at: :desc).limit(8)
   end
 
   def about; end
