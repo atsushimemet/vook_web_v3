c68dd29704e27b93c16e304aec712a0c38a6a88b
garammasala29 <kmasa1569@gmail.com>
2023-08-20 13:45:12 +0900

    fix: #101 HEADED=trueをつけることでブラウザテストができるよう修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,9 @@
 RSpec.configure do |config|
   config.before type: :system do
-    driven_by :selenium, using: :headless_chrome
+    if ENV['HEADED']
+      driven_by :selenium, using: :chrome
+    else
+      driven_by :selenium, using: :headless_chrome
+    end
   end
 end
