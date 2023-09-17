d688c2ebe63f672c535b7b883900e55353de21d1
garammasala29 <kmasa1569@gmail.com>
2023-09-17 20:12:20 +0900

    close #123, close #131
    * bug: #123 ブラウザバックのたびにSplideインスタンスが作られないよう修正
    
    * style: #129 ヘッダー下にラインを追加
    
    * style: #129 カルーセルの矢印を下部に表示させるよう修正
    
    * bug: #131 ブラウザバックのたびにChoicesインスタンスが作られないよう修正
    
    * fix: N+1問題の解消

--- app/javascript/knowledge-carousel.js
+++ app/javascript/knowledge-carousel.js
@@ -1,30 +1,39 @@
 import Splide from '@splidejs/splide';
 
+let splideInstance;
+
+const splideOptions = {
+  type: 'loop',
+  perPage: 3,
+  perMove: 1,
+  rewind: true,
+  gap: '2rem',
+  padding: '6rem',
+  focus: 'center',
+  lazyLoad: 'nearby',
+  breakpoints: {
+    768: {
+      type: 'slide',
+      perPage: 2,
+      perMove: 5,
+      rewind: false,
+      padding: 0,
+      focus: 1,
+      gap: '0.5rem',
+      omitEnd: true,
+      arrows: false,
+    },
+  },
+};
+
 document.addEventListener('turbo:load', () => {
   const splide = document.querySelector('.splide');
+
+  if (splideInstance) {
+    splideInstance.destroy();
+  }
+
   if (splide) {
-    new Splide('.splide', {
-      type: 'loop',
-      perPage: 3,
-      perMove: 1,
-      rewind: true,
-      gap: '2rem',
-      padding: '6rem',
-      focus: 'center',
-      lazyLoad: 'nearby',
-      breakpoints: {
-        768: {
-          type: 'slide',
-          perPage: 2,
-          perMove: 5,
-          rewind: false,
-          padding: 0,
-          focus: 1,
-          gap: '0.5rem',
-          omitEnd: true,
-          arrows: false,
-        },
-      },
-    }).mount();
+    splideInstance = new Splide(splide, splideOptions).mount();
   }
 });
