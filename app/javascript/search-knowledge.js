fbd9bd9b3d20a7f9637c2e409cee4b5ae64d3139
garammasala29 <kmasa1569@gmail.com>
2023-09-30 11:12:41 +0900

    feat: #147 商品検索機能を追加

--- app/javascript/search-knowledge.js
+++ app/javascript/search-knowledge.js
@@ -1,17 +1,18 @@
 import Choices from 'choices.js';
 
-let choicesInstance;
+let choicesInstanceKnowledge;
+let choicesInstanceProduct;
 
 document.addEventListener('turbo:load', () => {
   const knowledgeSearch = document.querySelector('.knowledge-search');
 
-  if (choicesInstance) {
-    choicesInstance.destroy();
+  if (choicesInstanceKnowledge) {
+    choicesInstanceKnowledge.destroy();
   }
 
   if (knowledgeSearch) {
     knowledgeSearch.selectedIndex = 0;
-    choicesInstance = new Choices(knowledgeSearch, {
+    choicesInstanceKnowledge = new Choices(knowledgeSearch, {
       allowHTML: true,
       searchResultLimit: 10,
       noResultsText: '一致する情報は見つかりません',
@@ -22,6 +23,29 @@ document.addEventListener('turbo:load', () => {
       const selectedValue = this.value;
       if (selectedValue) {
         window.location.href = `/knowledges/${selectedValue}`;
+      }
+    });
+  }
+
+  const productSearch = document.querySelector('.product-search');
+
+  if (choicesInstanceProduct) {
+    choicesInstanceProduct.destroy();
+  }
+
+  if (productSearch) {
+    productSearch.selectedIndex = 0;
+    choicesInstanceProduct = new Choices(productSearch, {
+      allowHTML: true,
+      searchResultLimit: 10,
+      noResultsText: '一致する商品は見つかりません',
+      itemSelectText: '',
+    });
+
+    productSearch.addEventListener('change', function () {
+      const selectedValue = this.value;
+      if (selectedValue) {
+        window.location.href = `/products/${selectedValue}`;
       }
     });
   }
