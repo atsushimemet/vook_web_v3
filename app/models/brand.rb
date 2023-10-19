6bf7084d8ab21298f151ac6333e1318a7583c356
atsushimemet <noap3b69n@gmail.com>
2023-10-19 11:40:29 +0900

    fix: #188 brandを削除できるようにするため

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ApplicationRecord
   has_many :knowledges, dependent: :nullify
-  has_many :items, dependent: :nullify, through: :lines
   has_many :lines, dependent: :nullify
+  has_many :items, dependent: :nullify, through: :lines
   has_one_attached :image
   has_one_attached :banner
 
