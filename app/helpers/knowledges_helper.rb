b46734eea1e4292a60a46758bc953bdee16e2983
garammasala29 <kmasa1569@gmail.com>
2023-11-28 23:03:13 +0900

    close #241

--- app/helpers/knowledges_helper.rb
+++ app/helpers/knowledges_helper.rb
@@ -1,6 +1,6 @@
 module KnowledgesHelper
   def brand_select_options
-    Brand.order(:id).map { |brand| [brand.name, brand.id, { data: { children_path: brand_lines_path(brand) } }] }
+    Brand.order(:id).map { |brand| [brand.name, brand.id, { data: { children_path: brand_lines_path(brand.id) } }] }
   end
 
   def line_select_options(knowledge)
