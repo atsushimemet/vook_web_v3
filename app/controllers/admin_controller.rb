2aca9f244647be9bbcc159e9eaba67740ccfe514
garammasala29 <kmasa1569@gmail.com>
2024-01-02 16:22:34 +0900

    feat: #250 brand/itemにdisplay_order順に変更

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,8 +2,8 @@ class AdminController < ApplicationController
   before_action :require_admin_login, only: %i[show]
 
   def show
-    @brands = Brand.all
-    @items = Item.all
+    @brands = Brand.order(:display_order)
+    @items = Item.order(:display_order)
     @lines = Line.all
   end
 end
