89b0134d8427213e7de67d23521771e49b0a9dc9
garammasala29 <kmasa1569@gmail.com>
2024-02-17 11:22:48 +0900

    close #350
    * style: #350 フィード画像の調整
    
    * fix N+1解消

--- app/controllers/api/knowledges_controller.rb
+++ app/controllers/api/knowledges_controller.rb
@@ -1,7 +1,7 @@
 module API
   class KnowledgesController < ApplicationController
     def index
-      knowledges = Knowledge.all
+      knowledges = Knowledge.includes(:brand, :line).all
       render json: KnowledgeResource.new(knowledges)
     end
   end
