93254f641aa49b3477f1876c9a1a33d424516618
garammasala29 <kmasa1569@gmail.com>
2024-03-23 21:39:30 +0900

    close #377

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,6 +1,6 @@
 module MetaTagsHelper
   def default_meta_tags # rubocop:disable Metrics/MethodLength
-    site_name = 'Vook'
+    site_name = 'Vook tokyo'
     default_title = "#{site_name}｜古着・ヴィンテージを楽しむための新しいメディア"
     default_meta_description = 'ヴィンテージに特化したキュレーションメディアです。古着を楽しむための基本的な知識をわかりやすく解説'
     default_og_image = image_url('logo_ogp.png')
