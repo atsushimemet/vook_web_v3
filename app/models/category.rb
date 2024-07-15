1083e94d934f207221afbc5c50e1b1958eb98c3e
garammasala29 <kmasa1569@gmail.com>
2024-07-15 16:07:58 +0900

    close #481
    * fix: #482 横スクロールできないよう設定
    
    * fix: モバイル時にメニューが表示されるよう修正
    
    * fix: #481 uniqバリデーションを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :term_categories, dependent: :destroy
   has_many :terms, dependent: :nullify, through: :term_categories
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 end
