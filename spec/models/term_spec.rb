47c2ae82f33f99dd30a8b373dd576c5f78507a67
garammasala29 <kmasa1569@gmail.com>
2024-07-30 23:10:14 +0900

    fix: #491 KANAグループと並び替えの修正

--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Term, type: :model do
   describe 'grouped_by_kana' do
-    let(:term_akamimi) { create(:term) }
-    let(:term_migiaya) { create(:term, name: '右綾', kana: 'ミギアヤ') }
+    let!(:term_akamimi) { create(:term) }
+    let!(:term_migiaya) { create(:term, name: '右綾', kana: 'ミギアヤ') }
 
     it 'ア行の用語を取得できること' do
       expect(described_class.grouped_by_kana['ア行']).to include term_akamimi
