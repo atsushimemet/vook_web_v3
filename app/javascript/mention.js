ec1ae956a85a1da4f21bfcaa376e2b7b09b28b8b
garammasala29 <kmasa1569@gmail.com>
2024-08-01 23:12:51 +0900

    close #495
    * fix: #495 全てのtrix-editorでtermメンションが使えるよう修正
    
    * fix: #496 メンション末尾に空白をつけないよう設定
    
    * fix lint修正
    
    * Revert "fix: #496 メンション末尾に空白をつけないよう設定"
    
    This reverts commit fe2c371e4ad195905a092ccddc69b261e73843a8.

--- app/javascript/mention.js
+++ app/javascript/mention.js
@@ -1,27 +1,29 @@
 import Tribute from 'tributejs';
 
 document.addEventListener('turbo:load', () => {
-  const textarea = document.querySelector('.textarea');
+  const trixEditors = document.querySelectorAll('trix-editor');
 
-  if (textarea) {
-    fetch('/api/terms')
-      .then((response) => response.json())
-      .then((data) => {
-        const tribute = new Tribute({
-          collection: [
-            {
-              trigger: '@',
-              values: data,
-              lookup: 'name',
-              menuItemLimit: 10,
-              selectTemplate: (item) => {
-                return `<a href="/terms#${item.original.name}">${item.original.name}</a>`;
+  trixEditors.forEach((textarea) => {
+    if (textarea) {
+      fetch('/api/terms')
+        .then((response) => response.json())
+        .then((data) => {
+          const tribute = new Tribute({
+            collection: [
+              {
+                trigger: '@',
+                values: data,
+                lookup: 'name',
+                menuItemLimit: 10,
+                selectTemplate: (item) => {
+                  return `<a href="/terms#${item.original.name}">${item.original.name}</a>`;
+                },
               },
-            },
-          ],
+            ],
+          });
+          tribute.attach(textarea);
+          return tribute;
         });
-        tribute.attach(textarea);
-        return tribute;
-      });
-  }
+    }
+  });
 });
