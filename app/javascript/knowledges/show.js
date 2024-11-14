5b0360d6d7be509f762886a6e4f7de3d50aff955
garammasala29 <kmasa1569@gmail.com>
2024-11-14 20:51:37 +0900

    feat: #449 demoを表示

--- app/javascript/knowledges/show.js
+++ app/javascript/knowledges/show.js
@@ -1,3 +1,14 @@
 import '../knowledge-mermaid';
-import '../price-histogram';
 import '../clipboard';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import PriceHistogram from './PriceHistogram';
+
+ReactDOM.createRoot(document.getElementById('price-histogram')).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <PriceHistogram />
+    </StyledEngineProvider>
+  </React.StrictMode>,
+);
