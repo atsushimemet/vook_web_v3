0937c6502414f8755ea4359d031b3d5261a42862
garammasala29 <kmasa1569@gmail.com>
2025-04-14 15:18:36 +0900

    feat: #780 最新記事から並べるように修正

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -14,7 +14,7 @@ class MagazinesController < ApplicationController
     raise ActiveRecord::RecordNotFound unless @magazine.published? || admin_login?
 
     @related_magazines = Magazine.published.with_attached_thumbnail.tagged_with(@magazine.tag_list, any: true)
-                                 .where.not(id: @magazine.id).distinct.limit(4)
+                                 .where.not(id: @magazine.id).order(publish_at: :desc).distinct.limit(4)
   end
 
   def new
