059db324b02227a10f13df894356bd56d1589843
garammasala29 <kmasa1569@gmail.com>
2025-04-24 01:44:17 +0900

    feat: #736 API項目追加

--- app/controllers/api/magazines_controller.rb
+++ app/controllers/api/magazines_controller.rb
@@ -1,8 +1,20 @@
 module API
   class MagazinesController < ApplicationController
+    include Pagy::Backend
+
     def index
-      magazines = Magazine.all
-      render json: MagazineResource.new(magazines)
+      scope_magazines = if admin_login?
+                          Magazine.with_attached_thumbnail.order(publish_at: :desc)
+                        else
+                          Magazine.published.with_attached_thumbnail.order(publish_at: :desc)
+                        end
+
+      pagy, magazines = pagy(scope_magazines, limit: 16)
+
+      render json: {
+        magazines: MagazineResource.new(magazines),
+        next: pagy.next
+      }
     end
   end
 end
